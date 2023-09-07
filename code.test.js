ebde080a23bac354131e64f28dc60b39fbd9a00c
JustinQuanCC <justin.quan@mycc.caspercollege.edu>
2023-09-07 17:47:09 -0600

    Added a way to get first occurance of element if duplicates are present

--- code.test.js
+++ code.test.js
@@ -5,11 +5,11 @@ eval(fs.readFileSync('code.js')+'');
 
 const testSearch =
     jsc.forall("array nat", function(arr) {
+        arr.sort(function(a,b){return a-b}) // sorting the array so it actually works
         if(arr.length > 0) {
-            return binarySearch(arr, arr[0]) === 0;
+            return binarySearch(arr, arr[0]) == 0;
         } else {
-            return binarySearch(arr, "foo") === -1;
+            return binarySearch(arr, "foo") == -1;
         }
     });
-
 jsc.assert(testSearch);
